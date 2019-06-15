1f6b064f961d1be99eea04d0ad2b34a5f626fb64
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2019-06-15 18:29:04 +0430

    blogApp   - custom manager in models

--- blogProject/blogApp/models.py
+++ blogProject/blogApp/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Our Post Model
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -12,12 +15,18 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, related_name='blog_posts',on_delete='PROTECT')
+    author = models.ForeignKey(User, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    # The default manager
+    objects = models.Manager()
+
+    # Custom made manager
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
