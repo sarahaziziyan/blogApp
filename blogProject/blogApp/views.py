631b7dc812a0555b3b0b8182b854a7e99bd892a2
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2019-06-18 17:21:14 +0430

    blogApp   - it shows a page with posts in it !!!

--- blogProject/blogApp/views.py
+++ blogProject/blogApp/views.py
@@ -4,9 +4,9 @@ from .models import Post
 
 def post_list_view(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blogApp/post/list.html', {'posts': posts})
 
 
 def post_detail_view(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blogApp/post/detail.html', {'post': post})
